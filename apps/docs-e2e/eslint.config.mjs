52bbd4c6dee7b0e33012f87c0e2302ab3a7b8612
mofirojean <mofirojean@gmail.com>
2025-12-25 02:00:48 +0100

    fix deployment error with nx console

--- apps/docs-e2e/eslint.config.mjs
+++ apps/docs-e2e/eslint.config.mjs
@@ -2,11 +2,11 @@ import playwright from 'eslint-plugin-playwright';
 import baseConfig from '../../eslint.config.mjs';
 
 export default [
-  playwright.configs['flat/recommended'],
-  ...baseConfig,
-  {
-    files: ['**/*.ts', '**/*.js'],
-    // Override or add rules here
-    rules: {},
-  },
+    ...baseConfig,
+    playwright.configs['flat/recommended'],
+    {
+        files: ['**/*.ts', '**/*.js'],
+        // Override or add rules here
+        rules: {},
+    }
 ];
